4907812641151b952b5dc9c8da0e452bf03f4d54
Marvin-Alberts <marvin.alberts@ibm.com>
2023-08-23 10:12:25 +0200

    add support for molecule set

--- src/nmr_to_structure/prepare_input/prepare_nmr_rxn_input.py
+++ src/nmr_to_structure/prepare_input/prepare_nmr_rxn_input.py
@@ -4,8 +4,51 @@ import click
 import pandas as pd
 from rxn.chemutils.tokenization import tokenize_smiles
 from tqdm.auto import tqdm
+import random
 
 from .nmr_utils import build_cnmr_string, build_hnmr_string, save_set, split_data
+
+NON_MATCHING_TOKEN = "<no_match>"
+
+def make_nmr(mode: str, component: str, hnmr: dict = None, cnmr: dict = None, hnmr_mode: str = 'range', token_space: str = 'shared'):
+
+    if mode == "combined":
+        hnmr_string = build_hnmr_string(
+            smiles=component,
+            peak_dict=hnmr,
+            mode=hnmr_mode,
+            header=False,
+            token_space=token_space,
+        )[0]
+
+        cnmr_string = build_cnmr_string(
+            cnmr,
+            header=False,
+            token_space=token_space,
+        )[0]
+
+        nmr_string = " {} {}".format(hnmr_string.strip(), cnmr_string)
+
+    elif mode == "hnmr":
+        hnmr_string = build_hnmr_string(
+            smiles=component,
+            peak_dict=hnmr,
+            mode=hnmr_mode,
+            header=False,
+            token_space=token_space,
+        )[0]
+
+        nmr_string = " " + hnmr_string
+
+    elif mode == "cnmr":
+        cnmr_string = build_cnmr_string(
+            cnmr,
+            header=False,
+            token_space=token_space,
+        )[0]
+        nmr_string = " " + cnmr_string
+    
+    return nmr_string
 
 
 def make_nmr_rxn_set(
@@ -13,11 +56,13 @@ def make_nmr_rxn_set(
     rxn_set: pd.DataFrame,
     mode: str,
     hnmr_mode: str = "range",
-    token_space="shared",
-    not_include_formula=False,
+    token_space: str ="shared",
+    non_matching: bool = True
+
 ) -> pd.DataFrame:
     rxns = list(rxn_set.index)
     mols = set(nmr_set.index)
+    mols_sample = tuple(mols)
 
     src_tgt_pairs = list()
 
@@ -31,48 +76,83 @@ def make_nmr_rxn_set(
         for component in components:
             if component not in mols or len(component) < 5:
                 continue
-
+            
+            # Make matching set
             nmr_input = components_tokenized
+            nmr = make_nmr(mode, component, 
+                           hnmr=nmr_set.loc[component]["1H_NMR_sim"]["peaks"], 
+                           cnmr=nmr_set.loc[component]["13C_NMR_sim"], 
+                           hnmr_mode=hnmr_mode,
+                           token_space=token_space)
+            nmr_input = components_tokenized + ' ' + nmr
 
-            if mode == "combined":
-                hnmr_string = build_hnmr_string(
-                    smiles=component,
-                    peak_dict=nmr_set.loc[component]["1H_NMR_sim"]["peaks"],
-                    mode=hnmr_mode,
-                    header=not not_include_formula,
-                    token_space=token_space,
-                )[0]
+            src_tgt_pairs.append({"nmr_input": nmr_input, "smiles": component, 'actual_smiles': component})
 
-                cnmr_string = build_cnmr_string(
-                    nmr_set.loc[component]["13C_NMR_sim"],
-                    header=False,
-                    token_space=token_space,
-                )[0]
+            if not non_matching:
+                continue
 
-                nmr_input += " {} {}".format(hnmr_string.strip(), cnmr_string)
-
-            elif mode == "hnmr":
-                hnmr_string = build_hnmr_string(
-                    smiles=component,
-                    peak_dict=nmr_set.loc[component]["1H_NMR_sim"]["peaks"],
-                    mode=hnmr_mode,
-                    header=not not_include_formula,
-                    token_space=token_space,
-                )[0]
-
-                nmr_input += " " + hnmr_string
-            elif mode == "cnmr":
-                cnmr_string = build_cnmr_string(
-                    nmr_set.loc[component]["13C_NMR_sim"],
-                    header=False,
-                    token_space=token_space,
-                )[0]
-                nmr_input += " " + cnmr_string
-
-            src_tgt_pairs.append({"nmr_input": nmr_input, "smiles": component})
+            # Create non matching input
+            random_mol = component
+            while random_mol == component:
+                random_mol = random.choice(mols_sample)
+                
+            nmr = make_nmr(mode, component, 
+                           hnmr=nmr_set.loc[random_mol]["1H_NMR_sim"]["peaks"], 
+                           cnmr=nmr_set.loc[random_mol]["13C_NMR_sim"], 
+                           hnmr_mode=hnmr_mode,
+                           token_space=token_space)
+            nmr_input_non_matching = components_tokenized + ' ' + nmr
+            src_tgt_pairs.append({'nmr_input': nmr_input_non_matching, 'smiles': NON_MATCHING_TOKEN, 'actual_smiles': random_mol})
 
     return pd.DataFrame(src_tgt_pairs)
 
+
+def make_nmr_mol_set(nmr_df: pd.DataFrame,
+                     mode: str, 
+                     hnmr_mode: str,
+                     token_space: str,
+                     non_matching=True, 
+                     n_mol_max: int = 7, 
+                     n_samples: int = 1000):
+
+    mols = tuple(set(nmr_df.index))
+    src_tgt_pairs = list()
+
+    # Default mol_distribution
+    mol_distribution = [[i, n_samples] for i in range(2, n_mol_max+1)]
+
+    for n_mols, n_examples in tqdm.tqdm(mol_distribution):
+        for _ in range(n_examples):
+            # Create set of molecules
+            mol_set = random.sample(mols, k=n_mols)
+
+            # Matching set
+            nmr_mol = random.choice(mol_set)
+            nmr = make_nmr(mode, nmr_mol, 
+                           hnmr=nmr_df.loc[nmr_mol]["1H_NMR_sim"]["peaks"], 
+                           cnmr=nmr_df.loc[nmr_mol]["13C_NMR_sim"], 
+                           hnmr_mode=hnmr_mode,
+                           token_space=token_space)
+            nmr_input_matching = tokenize_smiles('.'.join(mol_set)) + ' ' + nmr
+            src_tgt_pairs.append({'nmr_input': nmr_input_matching, 'smiles': nmr_mol})
+
+            if not non_matching:
+                continue
+
+            # Create not matching set
+            random_mol = nmr_mol 
+            while random_mol in mol_set:
+                random_mol = random.choice(mols)
+
+            nmr = make_nmr(mode, nmr_mol, 
+                           hnmr=nmr_df.loc[random_mol]["1H_NMR_sim"]["peaks"], 
+                           cnmr=nmr_df.loc[random_mol]["13C_NMR_sim"], 
+                           hnmr_mode=hnmr_mode,
+                           token_space=token_space)
+            nmr_input_non_matching = tokenize_smiles('.'.join(mol_set)) + ' ' + nmr
+            src_tgt_pairs.append({'nmr_input': nmr_input_non_matching, 'smiles': NON_MATCHING_TOKEN})
+
+    return pd.DataFrame.from_dict(src_tgt_pairs)
 
 @click.command()
 @click.option(
@@ -115,7 +195,12 @@ def make_nmr_rxn_set(
     default="shared",
     help="Wether the token space between the 1H and 13C NMR is shared or not",
 )
-@click.option("--not_include_formula", is_flag=True, help="")
+@click.option(
+    "--non_matching",
+    is_flag=True,
+    help="Wether or not to include non matching examples.",
+)
+
 def main(
     nmr_data: Path,
     rxn_data: Path,
@@ -123,30 +208,63 @@ def main(
     mode: str,
     hnmr_mode: str = "range",
     token_space: str = "shared",
-    not_include_formula: bool = False,
+    non_matching: bool = True
 ):
     nmr_df = pd.read_pickle(nmr_data)
+    nmr_df.drop(columns=['1H_NMR_exp', '13C_NMR_exp'], inplace=True)
+    nmr_df.dropna(inplace=True)
+
     rxn_df = pd.read_pickle(rxn_data)
 
     # Make the training data
-    input_df = make_nmr_rxn_set(
+    input_rxn = make_nmr_rxn_set(
         nmr_df,
         rxn_df,
         mode,
         hnmr_mode=hnmr_mode,
         token_space=token_space,
-        not_include_formula=not_include_formula,
+        non_matching=non_matching
     )
 
     # Split into train, test, val on the molecules
-    all_smiles = pd.unique(input_df.smiles)
+    all_smiles = pd.unique(input_rxn.actual_smiles)
     train_smiles, test_smiles, val_smiles = split_data(all_smiles)
 
-    train_set = input_df[input_df.smiles.isin(train_smiles)]
-    test_set = input_df[input_df.smiles.isin(test_smiles)]
-    val_set = input_df[input_df.smiles.isin(val_smiles)]
+    train_set_rxn = input_rxn[input_rxn.actual_smiles.isin(train_smiles)]
+    test_set_rxn = input_rxn[input_rxn.actual_smiles.isin(test_smiles)]
+    val_set_rxn = input_rxn[input_rxn.actual_smiles.isin(val_smiles)]
+
+    # Create molecule set with approximately the same size as the rxn set
+    n_samples_train, n_samples_test, n_samples_val = int(len(train_set_rxn)/2/6), int(len(test_set_rxn)/2/6), int(len(val_set_rxn)/2/6)
+
+    train_set_mol = make_nmr_mol_set(nmr_df[nmr_df.index.isin(train_smiles)], mode,
+                                     hnmr_mode=hnmr_mode,
+                                     token_space=token_space,
+                                     non_matching=non_matching,
+                                     n_samples=n_samples_train)
+    
+    test_set_mol = make_nmr_mol_set(nmr_df[nmr_df.index.isin(test_smiles)], mode,
+                                     hnmr_mode=hnmr_mode,
+                                     token_space=token_space,
+                                     non_matching=non_matching,
+                                     n_samples=n_samples_test)
+
+    val_set_mol = make_nmr_mol_set(nmr_df[nmr_df.index.isin(val_smiles)], mode,
+                                        hnmr_mode=hnmr_mode,
+                                        token_space=token_space,
+                                        non_matching=non_matching,
+                                        n_samples=n_samples_val)
+    
+    # Concatenate training and validation set
+    combined_train_set = pd.concat((train_set_rxn, train_set_mol))
+    combined_val_set = pd.concat((val_set_rxn, val_set_mol))
 
     # Save training data
-    save_set(train_set, out_path, "train")
-    save_set(test_set, out_path, "test")
-    save_set(val_set, out_path, "val")
+    save_set(train_set_rxn, out_path, "train-rxn")
+    save_set(train_set_mol, out_path, "train-mol")
+    save_set(combined_train_set, out_path, 'train')
+
+    save_set(combined_val_set, out_path, "val")
+
+    save_set(test_set_mol, out_path, "test-mol")
+    save_set(test_set_rxn, out_path, "test-rxn")
